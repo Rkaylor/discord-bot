df9a704c8137db6a0b9af8f4852bb2c7f99ff8d4
robert kaylor <robertkaylor@roberts-MBP.home>
2021-02-04 22:14:24 -0500

    Added simple chat functions for the bot to be able to process.

--- src/bot.js
+++ src/bot.js
@@ -5,7 +5,27 @@ console.log(process.env.DISCORDJS_BOT_TOKEN);
 //Bot login function
 
 const { Client } = require('discord.js');
-
 const client = new Client();
+const PREFIX = "$";
+
+client.on('ready', () =>{
+    console.log('the bot has logged in.')
+    console.log(`${client.user.tag}`)
+});
+
+client.on('message', (message) => {
+    if (message.author.bot) return; 
+    if (message.content.startsWith(PREFIX)){
+        const [CMD_NAME, ...args] = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/);
+        
+        if(CMD_NAME === 'kick'){
+            message.channel.send('The User has bee kicked')
+        }
+        
+    }
+});
 
 client.login(process.env.DISCORDJS_BOT_TOKEN)
